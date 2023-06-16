d0da343fe24d40c4852fe32bf4392adce7c638f4
Damian Dominik <damian.dominik5@gmail.com>
2023-06-16 10:27:10 +0200

    feat: another table variant

---
+++ src/components/TailwindTable/TailwindTable.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react'
+import { BookData } from '../../types/global'
+import { useFetchData } from '../../hooks/useFetchData'
+
+type TableRowProps = {
+  bookData: BookData
+}
+
+const TableRow = ({ bookData }: TableRowProps) => {
+  const { id, title, authors, publisher, publishedDate, pageCount, categories, imageLinks } = bookDatas
+  return (
+    <tr>
+      <th>
+        <label>
+          <input type="checkbox" className="checkbox" />
+        </label>
+      </th>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div className="avatar">
+            <div className="mask mask-squircle w-12 h-12">
+              <img src="/tailwind-css-component-profile-3@56w.png" alt="Avatar Tailwind CSS Component" />
+            </div>
+          </div>
+          <div>
+            <div className="font-bold">Brice Swyre</div>
+            <div className="text-sm opacity-50">China</div>
+          </div>
+        </div>
+      </td>
+      <td>
+        Carroll Group
+        <br />
+        <span className="badge badge-ghost badge-sm">Tax Accountant</span>
+      </td>
+      <td>Red</td>
+      <th>
+        <button className="btn btn-ghost btn-xs">details</button>
+      </th>
+    </tr>
+  )
+}
+
+type TailwindTableProps = {
+  searchTerm: string
+}
+
+const TailwindTable = ({ searchTerm }: TailwindTableProps) => {
+
+  const [selectedBooksIds, setSelectedBooksIds] = useState<string[]>([])
+
+  const booksData: (BookData[] | undefined) = useFetchData(searchTerm);
+
+  const handleSelect = (id: string) => {
+    const selected = selectedBooksIds.find(selectedId => selectedId === id)
+    if (selected) {
+      const newSelectedBooksIds = selectedBooksIds.filter(selectedId => selectedId !== id)
+      setSelectedBooksIds(newSelectedBooksIds)
+    } else {
+      setSelectedBooksIds([...selectedBooksIds, id])
+    }
+  }
+
+  console.log('booksData', booksData)
+  return (
+    // <div className="overflow-x-auto">
+    <table className="table w-full">
+      {/* head */}
+      <thead>
+        <tr>
+          <th>
+            <label>
+              <input type="checkbox" className="checkbox" />
+            </label>
+          </th>
+          <th>Name</th>
+          <th>Job</th>
+          <th>Favorite Color</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {booksData && booksData.map(book => <TableRow key={book.id} bookData={book} />)}
+      </tbody>
+      {/* foot */}
+      <tfoot>
+        <tr>
+          <th></th>
+          <th>Name</th>
+          <th>Job</th>
+          <th>Favorite Color</th>
+          <th></th>
+        </tr>
+      </tfoot>
+
+    </table>
+    // </div>
+  )
+}
+
+export default TailwindTable
